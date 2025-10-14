ef90f46e1c305eefd199e2bc446cca05ad351037
Dmitriy Latkin <lda020504@gmail.com>
2025-10-14 11:40:14 +0700

    верстка карточки новостей

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import cn from 'classnames';
 
 import style from './Card.module.scss';
+import {Link} from "react-router";
 
 interface Props {
   className?: string;
@@ -9,8 +10,36 @@ interface Props {
 
 export const Card: React.FC<Props> = ({className}) => {
   return (
-    <div className={cn(style.wrapper, className)}>
-      Card
-    </div>
+    <Link
+      className={cn(style.card, className)}
+      to={'/'}
+    >
+      <img
+        className={style.image}
+        width={340}
+        height={190}
+        src={'https://www.nj.com/resizer/v2/TLUFU3C4UJBRLB42JIZXRJM4LE.jpeg?auth=2aaaede8289d723aa433a1063eef632afab0e7ab7cacdd05981590ac51bc2520&width=1280&smart=true&quality=90'}
+        alt={''}
+        loading="lazy"
+      />
+      <h2 className={style.title}>Legendary Jersey Shore nightclub announces closure, final days of operation - NJ.com</h2>
+      <p className={style.description}>The popular Jersey Shore bar will be demolished for a waterfront redevelopment project including a hotel, residential buildings and retail space.</p>
+      <div className={style.bottom}>
+        <div className={style.bottomLeft}>
+          <span className={style.author}>N.J. News Report</span>
+          <span className={style.date}>12.10.2025</span>
+        </div>
+        <div className={style.bottomRight}>
+          <span>Source:</span>
+          <a
+            className={style.source}
+            href={'https://www.nj.com/monmouth/2025/10/legendary-jersey-shore-nightclub-announces-closure-final-days-of-operation.html'}
+            target="_blank"
+          >
+            nj.com
+          </a>
+        </div>
+      </div>
+    </Link>
   )
 }
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {HomeContextDispatch, HomeContextState,} from "@/state/home-reducer.ts";
-import {Categories} from '@/components';
+import {Card, Categories} from '@/components';
 import {useHomeReducer, useTitle} from "@/hooks";
+import style from "./HomePage.module.scss";
 
 export const HomePage: React.FC = () => {
   const [state, dispatch] = useHomeReducer();
@@ -10,7 +11,10 @@ export const HomePage: React.FC = () => {
   return (
     <HomeContextState.Provider value={state}>
       <HomeContextDispatch.Provider value={dispatch}>
-        <Categories />
+        <Categories className={style.categories} />
+        <div className={style.cardBox}>
+          <Card />
+        </div>
       </HomeContextDispatch.Provider>
     </HomeContextState.Provider>
   )
