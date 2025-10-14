da91703609334e3e58ffec19db1a7ddc95598032
Dmitriy Latkin <lda020504@gmail.com>
2025-10-14 11:59:42 +0700

    правка карточки новости;

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,20 +10,22 @@ interface Props {
 
 export const Card: React.FC<Props> = ({className}) => {
   return (
-    <Link
-      className={cn(style.card, className)}
-      to={'/'}
-    >
-      <img
-        className={style.image}
-        width={340}
-        height={190}
-        src={'https://www.nj.com/resizer/v2/TLUFU3C4UJBRLB42JIZXRJM4LE.jpeg?auth=2aaaede8289d723aa433a1063eef632afab0e7ab7cacdd05981590ac51bc2520&width=1280&smart=true&quality=90'}
-        alt={''}
-        loading="lazy"
-      />
-      <h2 className={style.title}>Legendary Jersey Shore nightclub announces closure, final days of operation - NJ.com</h2>
-      <p className={style.description}>The popular Jersey Shore bar will be demolished for a waterfront redevelopment project including a hotel, residential buildings and retail space.</p>
+    <div className={style.card}>
+      <Link
+        className={cn(style.link, className)}
+        to={'/'}
+      >
+        <img
+          className={style.image}
+          width={340}
+          height={190}
+          src={'https://www.nj.com/resizer/v2/TLUFU3C4UJBRLB42JIZXRJM4LE.jpeg?auth=2aaaede8289d723aa433a1063eef632afab0e7ab7cacdd05981590ac51bc2520&width=1280&smart=true&quality=90'}
+          alt={''}
+          loading="lazy"
+        />
+        <h2 className={style.title}>Legendary Jersey Shore nightclub announces closure, final days of operation - NJ.com</h2>
+        <p className={style.description}>The popular Jersey Shore bar will be demolished for a waterfront redevelopment project including a hotel, residential buildings and retail space.</p>
+      </Link>
       <div className={style.bottom}>
         <div className={style.bottomLeft}>
           <span className={style.author}>N.J. News Report</span>
@@ -35,11 +37,13 @@ export const Card: React.FC<Props> = ({className}) => {
             className={style.source}
             href={'https://www.nj.com/monmouth/2025/10/legendary-jersey-shore-nightclub-announces-closure-final-days-of-operation.html'}
             target="_blank"
+            onClick={() => console.log(1)}
           >
             nj.com
           </a>
         </div>
       </div>
-    </Link>
+
+    </div>
   )
 }
